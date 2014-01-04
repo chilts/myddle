029d3642e459bfaa941bde7aeb249c49657c252c
Andrew Chilton <andychilton@gmail.com>
2014-01-05 12:02:17 +1300

    Remove all debugging

--- myddle.js
+++ myddle.js
@@ -9,11 +9,6 @@
 // ----------------------------------------------------------------------------
 
 module.exports = function(context, fns, callback) {
-    console.log('1 - ' + typeof context);
-    console.log('1 - ' + Array.isArray(context));
-    console.log('2 - ' + typeof fns);
-    console.log('3 - ' + typeof callback);
-
     // if context isn't given
     if ( Array.isArray(context) ) {
         callback = fns;
@@ -25,36 +20,19 @@ module.exports = function(context, fns, callback) {
         callback = context;
         context = {};
     }
-    console.log('1 - ' + typeof context);
-    console.log('1 - ' + Array.isArray(context));
-    console.log('2 - ' + typeof fns);
-    console.log('3 - ' + typeof callback);
-
-    fns.forEach(function(fn) {
-        console.log(fn);
-        console.log(fn.length);
-    });
 
     // call each function (of length 1 or 2) in turn
     var upToIndex = -1;
     var err = null;
 
     function doNext() {
-        console.log('doNext(): entry');
-
         // incremement to the next function
         upToIndex = upToIndex + 1;
 
         // we're finished since there are no more fns
         if ( upToIndex === fns.length ) {
-            console.log('doNextError(): calling callback');
             return callback(err, context);
         }
-
-        console.log('- fns:', fns);
-        console.log('- upToIndex:', upToIndex);
-        console.log('- length1:', fns[upToIndex].length);
-        console.log('- length2:', fns.length);
 
         // if this is an error function, just skip to the next one
         // since we're not in an error state
@@ -64,18 +42,12 @@ module.exports = function(context, fns, callback) {
             });
         }
 
-        console.log('upToIndex:', upToIndex);
-
         // call this function with the context
-        console.log('doNext(): calling next myddleware at ' + upToIndex);
         fns[upToIndex](context, function(thisErr) {
             if (thisErr) {
-                console.log('doNext(): got an error after calling the regular myddleware');
                 err = thisErr;
                 return doNextError();
             }
-
-            console.log('doNext(): no error after calling the regular myddleware');
 
             // no error, so continue back on the regular chain and clear the error
             err = null;
@@ -84,21 +56,13 @@ module.exports = function(context, fns, callback) {
     }
 
     function doNextError() {
-        console.log('doNextError(): entry');
-
         // incremement to the next function
         upToIndex = upToIndex + 1;
 
         // we're finished since there are no more fns
         if ( upToIndex === fns.length ) {
-            console.log('doNextError(): calling callback');
             return callback(err, context);
         }
-
-        console.log('- fns:', fns);
-        console.log('- upToIndex:', upToIndex);
-        console.log('- length1:', fns[upToIndex].length);
-        console.log('- length2:', fns.length);
 
         // if this is a normal function, just skip to the next one
         // since we're currently in an error state
@@ -108,18 +72,13 @@ module.exports = function(context, fns, callback) {
             });
         }
 
-        console.log('upToIndex:', upToIndex);
-
         // call this function with the err and the context
-        console.log('doNextError(): calling next myddleware at ' + upToIndex);
         fns[upToIndex](err, context, function(thisErr) {
             if (thisErr) {
-                console.log('doNextError(): got an error after calling the error myddleware');
                 err = thisErr;
                 return doNextError();
             }
             // no error, so continue back on the regular chain and clear the error
-            console.log('doNextError(): No error after calling the error myddleware');
             err = null;
             doNext();
         });
